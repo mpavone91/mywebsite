f42fd1c719ecf78e9a6ee5583927f6075522b676
massimo <massimopavone@gmail.com>
2017-11-27 02:07:43 +0100

    added hi tag  Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   app/assets/scripts/app.js
    	new file:   app/assets/scripts/modules/revealonscroll.js
    	new file:   app/assets/styles/modules/_reveal-item.css
    	modified:   app/assets/styles/styles.css
    	modified:   app/tempo/scripts/app.js
    	modified:   app/tempo/styles/styles.css
    	modified:   package.json
    git commit -m 'completed'

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,3 +1,8 @@
 import MobileMenu from './modules/mobilemenu';
+import Revealonscroll from './modules/revealonscroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
+new Revealonscroll($(".feature-item"), "85%");
+new Revealonscroll($(".testimonial"), "75%");
+new Revealonscroll($(".generic-content-container"), "80%");
