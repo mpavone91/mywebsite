5c30c1ba95b74c3506d8c00aeb99564f660ef8bd
massimo <massimopavone@gmail.com>
2017-12-02 01:15:06 +0100

    completed modal

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -2,9 +2,11 @@ import MobileMenu from './modules/mobilemenu';
 import Revealonscroll from './modules/revealonscroll';
 import $ from 'jquery';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 var mobileMenu = new MobileMenu();
 new Revealonscroll($(".feature-item"), "85%");
 new Revealonscroll($(".testimonial"), "75%");
 new Revealonscroll($(".generic-content-container"), "80%");
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
